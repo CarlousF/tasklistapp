cde29956af960d65437d54dd2e4e4b8840417655
Carlous Fahmi <carlousf@hotmail.com>
2015-02-01 19:00:07 +0000

    Initial commit of "Task List" iOS App, developed in Swift.

--- My Task List/FirstViewController.swift
+++ My Task List/FirstViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tblTasks: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,33 @@ class FirstViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Returning to view
+    override func viewWillAppear(animated: Bool) {
+       tblTasks.reloadData()
+    }
+    
+    //UITableViewDelegate
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        if(editingStyle == UITableViewCellEditingStyle.Delete){
+            taskMGR.tasks.removeAtIndex(indexPath.row)
+            tblTasks.reloadData()
+        }
+    }
+    
+    
+    //UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskMGR.tasks.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "test")
+        cell.textLabel?.text = taskMGR.tasks[indexPath.row].name;
+        cell.detailTextLabel?.text = taskMGR.tasks[indexPath.row].desc;
+        return cell
     }
 
 
---
+++ My Task List/TaskManager.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var taskMGR: TaskManager = TaskManager()
+
+struct task{
+    var name = "Un-named"
+    var desc = "Un-Described"
+}
+
+class TaskManager: NSObject {
+    
+    var tasks = [task]()
+    
+    func addTask (name: String, desc: String) {
+        tasks.append(task(name: name, desc: desc))
+    }
+    
+    
+    
+}
